5c2df80adb92a33bc7c8f5c0c5357094a18bf08a
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-23 11:31:35 +0300

    Implement city search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Search from './components/search/search';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
   return (
-    <div className="App">
-      hello
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0d396cf9e5msh8c8649613a365b8p12b6e3jsn3d989ad9b458',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
