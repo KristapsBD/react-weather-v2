64b130bb58db2ebf566ffe9a6b5fa52c3d07c42b
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-23 13:20:59 +0300

    Change site title

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
 import Forecast from './components/forecast/forecast';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Weather app"
+  }, [])
 
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
